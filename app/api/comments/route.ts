90d72c6ed35872fadb5f2753f07b5d813ee8c3ce
alcel <alcel@rschooltoday.com>
2023-10-23 14:16:03 +0800

    Handle errors

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -3,15 +3,15 @@ import { z } from 'zod';
 import prisma from "@/prisma/client";
 
 const createCommentSchema = z.object({
-    description: z.string().min(1).max(255),
-    comment: z.string().min(1)
+    description: z.string().min(1, 'Description is required').max(255),
+    comment: z.string().min(1, 'Description is comment')
 })
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createCommentSchema.safeParse(body);
     if(!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400 });
+        return NextResponse.json(validation.error.format, { status: 400 });
 
     const newComment = await prisma.comments.create({
         data: { description: body.description, comment: body.comment }
--- app/comments/new/page.tsx
+++ app/comments/new/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 interface CommentForm {
     description: string;
@@ -15,26 +16,37 @@ interface CommentForm {
 function NewCommentPage() {
   const router = useRouter();
   const {register, control, handleSubmit} = useForm<CommentForm>();
-  console.log(register('description'));
+  const [error, setError] = useState('');
 
   return (
-    <form 
-        className='max-w-xl space-y-3' 
-        onSubmit={handleSubmit(async (data) => {
-            await axios.post('/api/comments', data);
-            router.push('/comments');
-        })}>
-        <TextField.Root>
-            <TextField.Input placeholder='Description' {...register('description')} />
-        </TextField.Root>
-        <Controller
-            name="comment"
-            control={control}
-            render={({ field }) => <SimpleMDE placeholder='Post comment' {...field}/>}>
-        </Controller>
-        
-        <Button>Submit Post</Button>
-    </form>
+    <div className='max-w-xl'>
+        {error && (
+            <Callout.Root color='red' className='mb-5'>
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>
+        )}
+        <form 
+            className='space-y-3' 
+            onSubmit={handleSubmit(async (data) => {
+                try {
+                    await axios.post('/api/comments', data);
+                    router.push('/comments');
+                } catch (error) {
+                    setError('An unexpected error occured.');
+                }
+            })}>
+            <TextField.Root>
+                <TextField.Input placeholder='Description' {...register('description')} />
+            </TextField.Root>
+            <Controller
+                name="comment"
+                control={control}
+                render={({ field }) => <SimpleMDE placeholder='Post comment' {...field}/>}>
+            </Controller>
+            
+            <Button>Submit Post</Button>
+        </form>
+    </div>
   )
 }
 
