4e817c6c9ced4a107e37d2ec24e4af3bc441ca33
alcel <alcel@rschooltoday.com>
2023-10-23 12:48:08 +0800

    Handle form submission

--- app/comments/new/page.tsx
+++ app/comments/new/page.tsx
@@ -2,17 +2,39 @@
 
 import { Button, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+
+interface CommentForm {
+    description: string;
+    comment: string;
+}
 
 function NewCommentPage() {
+  const router = useRouter();
+  const {register, control, handleSubmit} = useForm<CommentForm>();
+  console.log(register('description'));
+
   return (
-    <div className='max-w-xl space-y-3'>
+    <form 
+        className='max-w-xl space-y-3' 
+        onSubmit={handleSubmit(async (data) => {
+            await axios.post('/api/comments', data);
+            router.push('/comments');
+        })}>
         <TextField.Root>
-            <TextField.Input placeholder='Description' />
+            <TextField.Input placeholder='Description' {...register('description')} />
         </TextField.Root>
-        <SimpleMDE placeholder='Post comment' />
+        <Controller
+            name="comment"
+            control={control}
+            render={({ field }) => <SimpleMDE placeholder='Post comment' {...field}/>}>
+        </Controller>
+        
         <Button>Submit Post</Button>
-    </div>
+    </form>
   )
 }
 
